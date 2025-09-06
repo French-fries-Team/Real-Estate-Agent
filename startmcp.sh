349649c0ddf66b523fd421dc3d710cf448b5b55c
gaojia <sofia7452@163.com>
2025-09-06 23:51:54 +0800

    feat: 暂存

--- startmcp.sh
+++ startmcp.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 npm install -g @antv/mcp-server-chart
-# 使用 nohup 和 & 将进程放到后台运行
-nohup mcp-server-chart --transport sse > mcp.log 2>&1 &
-# 保存进程ID
-echo $! > .mcp.pid
+no_proxy="localhost,127.0.0.1" mcp-server-chart --transport sse
